c763165cc0c5be6c1afe49d7dd646dee88696bd7
thiago-ivasaki <thiagoivasaki@gmail.com>
2023-02-01 01:24:34 -0300

    feat: Criação da tabela users

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,42 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING(100),
+  },
+  role: {
+    type: STRING(100),
+  },
+  email: {
+    type: STRING(100),
+    allowNull: false,
+  },
+  password: {
+    type: STRING(100),
+    allowNull: false,
+  },
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  // modelName: 'example',
+  timestamps: false,
+});
+
+export default User;
