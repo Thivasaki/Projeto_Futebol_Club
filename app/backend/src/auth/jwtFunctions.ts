91c9d0cf0dc7eab194127e53eea84d1f61504f63
thiago-ivasaki <thiagoivasaki@gmail.com>
2023-02-01 21:51:19 -0300

    fix: mudança de tipo any do createToken para IUserLogin

--- app/backend/src/auth/jwtFunctions.ts
+++ app/backend/src/auth/jwtFunctions.ts
@@ -1,8 +1,9 @@
 import { sign } from 'jsonwebtoken';
+import { IUserLogin } from '../interfaces';
 
 const secret: string = process.env.JWT_SECRET || 'suaSenha';
 
-const createToken = (user: string): string => {
+const createToken = (user: IUserLogin): string => {
   const token = sign({ data: user }, secret, {
     algorithm: 'HS256',
     expiresIn: '1d',
