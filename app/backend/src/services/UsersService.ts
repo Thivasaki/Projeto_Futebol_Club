054e01a70f2dfbf02675e8f9943ca04c63cfe2ec
thiago-ivasaki <thiagoivasaki@gmail.com>
2023-02-10 16:06:09 -0300

    feat: Adição de validação de senha para a rota "Login"

--- app/backend/src/services/UsersService.ts
+++ app/backend/src/services/UsersService.ts
@@ -1,3 +1,4 @@
+import * as bcrypt from 'bcryptjs';
 import { IUserLogin, TRes } from '../interfaces';
 import { createToken } from '../auth/jwtFunctions';
 import User from '../database/models/UserModel';
@@ -6,11 +7,15 @@ export default class UserService {
   public userModel = User;
 
   public async login(user: IUserLogin): Promise<TRes> {
-    const { email } = user;
+    const { email, password } = user;
     const findUser = await this.userModel.findOne({ where: {
       email,
     } });
     if (!findUser) {
+      return { type: 'INVALID_INPUT', message: 'Incorrect email or password' };
+    }
+    const checkPassword = bcrypt.compareSync(password, findUser.dataValues.password);
+    if (!checkPassword) {
       return { type: 'INVALID_INPUT', message: 'Incorrect email or password' };
     }
     const token = createToken(user, findUser.role);
