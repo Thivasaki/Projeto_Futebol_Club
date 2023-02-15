a2426cba35ea7638601a949b7dc25713c61b146f
thiago-ivasaki <thiagoivasaki@gmail.com>
2023-02-15 20:23:23 -0300

    feat: Criação de arquivo para testes da rota login

---
+++ app/backend/src/tests/user.test.ts
@@ -0,0 +1,42 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import User from '../database/models/UserModel';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Seu teste', () => {
+
+  let chaiHttpResponse: Response;
+
+  // before(async () => {
+  //   sinon
+  //     .stub(Example, "findOne")
+  //     .resolves({
+  //       ...<Seu mock>
+  //     } as Example);
+  // });
+
+  // after(()=>{
+  //   (Example.findOne as sinon.SinonStub).restore();
+  // })
+
+  // it('...', async () => {
+  //   chaiHttpResponse = await chai
+  //      .request(app)
+  //      ...
+
+  //   expect(...)
+  // });
+
+  it('Seu sub-teste', () => {
+    expect(false).to.be.eq(true);
+  });
+});
