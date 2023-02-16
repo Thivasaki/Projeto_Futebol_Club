6751cfb3eb72c04f3ae2f3703172ae80a7d1f0a1
thiago-ivasaki <thiagoivasaki@gmail.com>
2023-02-15 22:35:21 -0300

    feat: Adição de testes de integração para a rota login

--- app/backend/src/tests/user.test.ts
+++ app/backend/src/tests/user.test.ts
@@ -4,39 +4,121 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import User from '../database/models/UserModel';
 
 import { Response } from 'superagent';
-
-chai.use(chaiHttp);
+import User from '../database/models/UserModel';
 
 const { expect } = chai;
+chai.use(chaiHttp);
 
-describe('Seu teste', () => {
+const mockUser = {
+  id: 1,
+  username: 'User',
+  role: 'user',
+  email: 'user@user.com',
+  password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO', 
+}
+
+describe('Testes da rota "/login"', () => {
 
   let chaiHttpResponse: Response;
 
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
-
   // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
+  //   (User.findOne as sinon.SinonStub).restore();
   // })
 
-  // it('...', async () => {
+  it('Teste para testar login feito com sucesso', async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves(mockUser as User);
+
+    const mockLogin = {
+      email: 'user@user.com',
+      password: 'secret_user',
+    }
+
+    chaiHttpResponse = await chai
+       .request(app)
+       .post('/login')
+       .send(mockLogin)
+
+    expect(chaiHttpResponse.status).to.have.equal(200);
+  });
+
+  // it('Teste para testar login feito com senha errada', async () => {
+  //   const mockLogin = {
+  //     email: 'user@user.com',
+  //     password: 'potato',
+  //   }
+
   //   chaiHttpResponse = await chai
   //      .request(app)
-  //      ...
+  //      .post('/login')
+  //      .send(mockLogin)
 
-  //   expect(...)
+  //   expect(chaiHttpResponse.status).to.have.equal(401);
   // });
 
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
-  });
+  // it('Teste para testar login feito com email errada', async () => {
+  //   const mockLogin = {
+  //     email: 'potato',
+  //     password: 'secret_user',
+  //   }
+
+  //   chaiHttpResponse = await chai
+  //      .request(app)
+  //      .post('/login')
+  //      .send(mockLogin)
+
+  //   expect(chaiHttpResponse.status).to.have.equal(401);
+  // });
+
+  // it('Teste para testar login feito sem senha', async () => {
+  //   const mockLogin = {
+  //     email: 'user@user.com',
+  //   }
+
+  //   chaiHttpResponse = await chai
+  //      .request(app)
+  //      .post('/login')
+  //      .send(mockLogin)
+
+  //   expect(chaiHttpResponse.status).to.have.equal(400);
+  // });
+
+  // it('Teste para testar login feito sem email', async () => {
+  //   const mockLogin = {
+  //     password: 'potato',
+  //   }
+
+  //   chaiHttpResponse = await chai
+  //      .request(app)
+  //      .post('/login')
+  //      .send(mockLogin)
+
+  //   expect(chaiHttpResponse.status).to.have.equal(400);
+  // });
+
+  // it('Teste para falha de autenticação de login', async () => {
+  //   const mockLogin = {
+  //     password: 'potato',
+  //   }
+
+  //   chaiHttpResponse = await chai
+  //      .request(app)
+  //      .get('/login/validate')
+
+  //   expect(chaiHttpResponse.status).to.have.equal(401);
+  // });
+
+  // it('Teste para falha de autenticação de login', async () => {
+  //   const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU0NTI3MTg5fQ.XS_9AA82iNoiVaASi0NtJpqOQ_gHSHhxrpIdigiT-fc'
+
+  //   chaiHttpResponse = await chai
+  //      .request(app)
+  //      .get('/login/validate')
+  //      .set('Authorization', token)
+
+  //   expect(chaiHttpResponse.status).to.have.equal(200);
+  // });
 });
