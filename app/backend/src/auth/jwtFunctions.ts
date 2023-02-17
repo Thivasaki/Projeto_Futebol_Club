ff14164a12d11d2af0526382f9b720d2fe99e7a1
thiago-ivasaki <thiagoivasaki@gmail.com>
2023-02-16 23:30:54 -0300

    fix: Mudança no texto da menssagem ao autenticar

--- app/backend/src/auth/jwtFunctions.ts
+++ app/backend/src/auth/jwtFunctions.ts
@@ -32,6 +32,6 @@ export const validateToken = async (req: Request, res: Response, next: NextFunct
     (req as CustomRequest).payload = decoded;
     next();
   } catch (err) {
-    return res.status(401).json({ message: 'Expired or invalid token' });
+    return res.status(401).json({ message: 'Token must be a valid token' });
   }
 };
